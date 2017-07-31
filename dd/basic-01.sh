b509f56e1db2f34f1598da8041d1dc9f5a561b68
Krishnan Mani <km@krishnanm.com>
2017-07-31 16:06:32 +0530

    Enhanced the script for dd

--- dd/basic-01.sh
+++ dd/basic-01.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-FILE_MOUNT_POINT="$1"
-FILE_NAME="$2"
-echo "Writing to ${FILE_MOUNT_POINT}/${FILE_NAME}"
-dd if=/dev/zero of=${FILE_MOUNT_POINT}/${FILE_NAME} bs=1G count=1 oflag=direct
+
+SCRIPT_NAME="$0"
+
+if [ $# -ne 4 ]
+then
+    echo "Error: missing argument(s)"
+    echo "Usage: ${SCRIPT_NAME} mount-point file-name-prefix block-size count"
+    exit 1
+fi
+
+MOUNT_POINT="$1"
+FILE_NAME_PREFIX="$2"
+BLOCK_SIZE="$3"
+COUNT="$4"
+
+FULL_FILE_NAME="${MOUNT_POINT}/${FILE_NAME_PREFIX}.${BLOCK_SIZE}.${COUNT}"
+
+echo "Writing to ${FULL_FILE_NAME}"
+dd if=/dev/zero of=${FULL_FILE_NAME} bs=${BLOCK_SIZE} count=${COUNT} oflag=direct
